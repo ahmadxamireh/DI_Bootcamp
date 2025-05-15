36607eb8337c5953eef969f19995ff50fd83902d
Ahmad Amireh <ahmad.ali.amireh@gmail.com>
2025-05-15 13:15:19 +0300

    D4 Timed Challenge #1

---
+++ W1/D4/ExercisesXP/TimedChallenge#1.py
@@ -0,0 +1,15 @@
+# Write a program which takes a string and a character as an input, and finds out the number of occurrences the
+# character has in the string.
+#
+# String: Programming is cool!
+# Character: o
+# 3
+#
+# String: This is a great example
+# Character: y
+# 0
+
+string = input('Enter a string: ')
+character = input('Enter a character: ')
+
+print(string.count(character))
