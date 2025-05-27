1aa6c5fcb8c9637a46cf51007c82acb6f7ab45f4
Ahmad Amireh <ahmad.ali.amireh@gmail.com>
2025-05-27 13:39:49 +0300

    W3-D1: Daily Challenge - Stars

--- W3/D1/DailyChallenge/C2/stars.js
+++ W3/D1/DailyChallenge/C2/stars.js
@@ -6,8 +6,7 @@
 
  Instructions
  1. Write a JavaScript program that recreates the pattern below.
- 2. Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the
- other - check out this tutorial of nested loops).
+ 2. Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other).
  3. Do this Daily Challenge by yourself, without looking at the answers on the internet.
  *
  * *
@@ -18,5 +17,13 @@
  */
 
 for (let i = 1; i < 7; i++) {
-    console.log("*".repeat(i))
+    console.log("* ".repeat(i).trim())
+}
+
+for (let i = 1; i < 7; i++) {
+    let stars = "";
+    for (let j = 0; j < i; j++) {
+        stars += "* ";
+    }
+    console.log(stars.trim());
 }
