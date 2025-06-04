804181eca0bb15e96ba3b50384a4af2465aba989
Ahmad Amireh <ahmad.ali.amireh@gmail.com>
2025-06-04 18:38:27 +0300

    W3-D4: Exercises XP - Find the Sum

---
+++ W3/D4/exercises/xp/find-the-sum.js
@@ -0,0 +1,7 @@
+// 🌟 Exercise 4: Find the sum
+// Instructions
+// Create a one-line function (i.e., an arrow function) that receives two numbers as parameters and returns the sum.
+
+const findTheSum = (a, b) => a + b;
+
+console.log(findTheSum(3, 7));
