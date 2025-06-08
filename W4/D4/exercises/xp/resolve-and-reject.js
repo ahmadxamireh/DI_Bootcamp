00aadae9bcfef170d5bfe4084f56a6a74b91395f
Ahmad Amireh <ahmad.ali.amireh@gmail.com>
2025-06-08 21:27:46 +0300

    W4-D4: Exercises XP - Resolve and Reject

---
+++ W4/D4/exercises/xp/resolve-and-reject.js
@@ -0,0 +1,8 @@
+// 🌟 Exercise 3: Resolve & Reject
+// 1. Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
+
+Promise.resolve(3).then(result => console.log(result));
+
+// 2. Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”
+
+Promise.reject('Boo!').catch(error => console.log(error));
