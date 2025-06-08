70db29e5705230c57731f4c1ffe47c664b2731b5
Ahmad Amireh <ahmad.ali.amireh@gmail.com>
2025-06-08 13:51:14 +0300

    W4-D1: Exercises XP Ninja - Email

---
+++ W4/D1/exercises/xp-ninja/email.js
@@ -0,0 +1,6 @@
+// Exercise 2: Email
+// Clean up this email to have no whitespaces. Do it in a single line (return a new string).
+
+const userEmail3 = ' cannotfillemailformcorrectly@gmail.com '
+const trimmedEmail = userEmail3.trim();
+console.log(trimmedEmail);
