bd101d6804789af6f23396b93bd24dbf69a0fe9e
Ahmad Amireh <ahmad.ali.amireh@gmail.com>
2025-07-05 17:32:55 +0300

    W6-D4: Daily Challenge - Task 1

---
+++ W6/D4/daily-challenge/app.js
@@ -0,0 +1,8 @@
+// 4. Create another file named app.js in the same directory.
+// 5. In app.js, require the greeting.js module and use the greet function to greet a user.
+
+const greet = require('./greeting.js');
+
+// test
+
+console.log(greet('Ahmad'));
---
+++ W6/D4/daily-challenge/greeting.js
@@ -0,0 +1,11 @@
+// Daily Challenge: Node.js App & NPM
+
+// Task 1:  Basic Module System
+// 1. Inside the daily-challenge directory, create a file named greeting.js.
+// 2. In greeting.js, define a function called greet that takes a name as a parameter and returns a personalized greeting message.
+
+const greet = name => `Hello, ${name}, Nice to see you here!`
+
+// 3. Export the greet function using the Node.js module system.
+
+module.exports = greet;
